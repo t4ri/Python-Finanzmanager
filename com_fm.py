1ee8b7f5154378d821ed323ae449b2d51520d0f1
t4ri <63642150+t4ri@users.noreply.github.com>
2023-02-10 14:04:15 +0100

    Add files via upload

---
+++ com_fm.py
@@ -0,0 +1,181 @@
+# t4ri, 2023
+import pdfplumber
+# https://github.com/jsvine/pdfplumber
+import re
+import os
+
+dir = ""
+year = ""
+booking_classes = {}
+booking_dict = {}
+
+def readini():
+    global dir, year, booking_classes
+    pre = re.compile(r'([0-9a-zA-ZäüöÄÜÖ_/]*) : (.*){1}(,(.*)){0,}')
+    ini = open("com_fm.ini", 'r')
+    line = ini.readline()
+    while len(line) > 0:
+        res = line.split(" : ")
+        if len(res) == 2:
+            item = res[0].rstrip()
+            if item[0] != "#":
+                if item == "pfad":
+                    dir = res[1].lstrip(" ").rstrip()
+                elif item == "jahr":
+                    year = res[1].lstrip(" ").rstrip()
+                else:
+                    name = res[0]
+                    values = res[1].lstrip(" ").rstrip().lower()
+                    values = values.replace(",,",",")
+                    if values[-1] == ",":
+                        values = values[:-1]
+                    res = values.split(",")                 
+                    booking_classes[name]=res
+        line = ini.readline()    
+    ini.close()
+    # Sammler Einnahmen 
+    booking_classes["Einnahmen div."]="+"
+    # Sammler Ausgaben     
+    booking_classes["Ausgaben div."]="-"
+
+# string in float Konvertierung
+def str2float(val):
+    s = val.replace(".","")
+    s = s.replace("+","")
+    s = s.replace(",",".")
+    return float(s)
+  
+# Zuordnung der Buchungsgruppe  
+def findclass(name, val):
+    for key in booking_classes:
+        cl = booking_classes[key]
+        if isinstance(cl, str):
+            # Sammler
+            v = str2float(val)
+            if (cl == "+") and (v >= 0):
+                return key
+            elif (cl == "-") and (v < 0):
+                return key
+        else:    
+            for attribute in cl:
+                if name.find(attribute) >= 0:
+                    # Schlüsselwort gefunden
+                    return key
+    return None
+    
+
+def readPDF(fname):
+    # Öffne das PDF Dokument
+    pdf = pdfplumber.open(fname)
+    # Regulärer Filter
+    #02.06.2022 Lastschrift/ STADTWERKE KD.-NR.346,00-ABSCHLAG0 -91,00
+    preval = re.compile(r'(?P<date>[0-9]{2}.[0-9]{2}.[0-9]{4}) (?P<type>[a-zA-ZäüöÄÜÖ]*)/{0,1} (?P<text>.*) (?P<value>[+,-]([0-9]{1,3}.{0,1})*,[0-9]{2})$')
+    # AlterSaldo 01.06.2022 +1.704,05
+    presaldo = re.compile(r'(?P<saldo>[a-zA-Z]*Saldo) (?P<date>[0-9]{2}.[0-9]{2}.[0-9]{4}) (?P<value>[+,-]([0-9]{1,3}.{0,1})*,[0-9]{2})$')
+    p = 1
+    stop = False
+    saldo = 0;
+    for page in pdf.pages:
+        # start ab Seite 2
+        if p>1:
+            # Seitentext
+            pagetext = page.extract_text()
+            lines = pagetext.split("\n")
+            for line in lines:
+                # Textzeile
+                info = ""
+                if preval.search(line):
+                    # Regular expression Datenzeile
+                    info = str(preval.search(line).groupdict()) 
+                    saldo += str2float(preval.search(line).group("value"))
+                    name = preval.search(line).group("text")
+                    typ = preval.search(line).group("type")
+                    # Name festlegen
+                    class_name = name+" "+typ
+                    class_name = class_name.lower()
+                    # Wertbuchung und Zuordnung
+                    booking_item = {}
+                    booking_item["date"] = preval.search(line).group("date")
+                    booking_item["name"] = class_name
+                    val = preval.search(line).group("value")
+                    booking_item["value"] = val
+                    booking_items = []
+                    overview_name = findclass(class_name,val)
+                    if  overview_name == None:
+                        overview_name = "unbekannt"                  
+                    olist = []
+                    if overview_name in booking_dict:
+                        olist = booking_dict[overview_name]
+                    olist.append(booking_item)
+                    booking_dict[overview_name] = olist   
+                                                
+                elif presaldo.search(line):
+                    # Regular expression Saldo
+                    info = str(presaldo.search(line).groupdict())
+                    saldotyp = presaldo.search(line).group("saldo")
+                    if (saldotyp == "AlterSaldo"):
+                        saldo = str2float(presaldo.search(line).group("value"))
+                    if (saldotyp == "NeuerSaldo"):
+                        neuersaldo = str2float(presaldo.search(line).group("value"))
+                        saldo = round(saldo,2)
+                        delta = neuersaldo - saldo
+                        # Saldoabweichung
+                        print(fname + " saldo delta "+str(delta))
+                        stop = True
+                if stop:
+                    break
+            if stop:
+                break
+        p += 1
+    pdf.close()
+
+    
+
+readini()
+if len(dir) == 0:   
+    print("ini Datei fehlerhaft")
+    exit()
+    
+files = os.listdir(dir) 
+if len(files) == 0:
+    print("keine Finanzreports gefunden")
+    exit()
+
+# PDF auswerten
+for file in files:
+    if (file.find(year) >= 0) and (file.find("Finanzreport") >= 0):
+        readPDF(dir + "\\" + file)
+
+# csv Datenexport
+res = open(year + "_finmanger.csv", 'w')
+bres = open(year + "_finmanger_bookings.csv", 'w')
+saldo=0
+for key in booking_dict.keys():
+    l = 0
+    sum = 0
+    for booking in booking_dict[key]:
+        sum += str2float(booking["value"])
+        bres.write(booking["date"]+";"+key+";"+booking["name"]+";"+booking["value"]+"\n")
+        l += 1
+    sum = round(sum,2)
+    saldo += sum
+    t = year + ";" + key + ";" + str(l) + ";" + str(sum)
+    print (t)
+    res.write(t+ "\n")
+saldo = round(saldo,2)
+t = year + ";Saldo;1;" + str(saldo)
+print (t)
+res.write(t+ "\n")
+res.close()
+bres.close()
+
+s="Einnahmen div."
+print("\n-----------------------------\n"+s+":")
+for booking in booking_dict[s]:
+    print (booking["date"]+", "+booking["name"]+", "+booking["value"])
+    
+s="Ausgaben div."
+print("\n-----------------------------\n"+s+":")
+for booking in booking_dict[s]:
+    print (booking["date"]+", "+booking["name"]+", "+booking["value"])
+    
